5d7162a502eee6f9f1cfa6266d7cff04d34acda2
kapilkushwah01 <kapilkushwah05@gmail.com>
2025-06-09 19:31:28 +0530

    setup expo router

---
+++ src/app/index.tsx
@@ -0,0 +1,10 @@
+import "../../global.css"
+import {View,Text} from "react-native";
+
+export default function HomePage(){
+    return(
+        <View className="flex-1 items-center justify-center ">
+            <Text>This is home page</Text>
+        </View>
+    )
+}
