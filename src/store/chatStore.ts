af503416e9c444d636f57a6df2e798a095fa6b6e
kapilkushwah01 <kapilkushwah05@gmail.com>
2025-06-25 18:36:15 +0530

    add global store

--- src/store/chatStore.ts
+++ src/store/chatStore.ts
@@ -1,32 +1,41 @@
-import { Chat,Message } from "@/types/types";
 import { create } from "zustand";
+import { Chat, Message } from "@/types/types";
+import { persist, createJSONStorage } from "zustand/middleware";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 interface ChatStore {
-    chatHistory:Chat[];
-    createNewChat:(title:string) =>string;
-    addNewMessage:(chatId:string,message:Message) =>void;
+    chatHistory: Chat[];
+    createNewChat: (title: string) => string;
+    addNewMessage: (chatId: string, message: Message) => void;
 }
-export const useChatStore = create<ChatStore>((set) =>({
-    chatHistory:[],
-    createNewChat:(title:string) => {
-        const newChat: Chat = {
-            id: Date.now().toString(),
-            title,
-            messages:[]
-        };
-        set((state)=>({
-            chatHistory:[newChat, ...state.chatHistory ]
-        }));
+export const useChatStore = create<ChatStore>()(
+    persist(
+        (set) => ({
+            chatHistory: [],
+            createNewChat: (title: string) => {
+                const newChat: Chat = {
+                    id: Date.now().toString(),
+                    title,
+                    messages: []
+                };
+                set((state) => ({
+                    chatHistory: [newChat, ...state.chatHistory]
+                }));
 
-        return newChat.id;
+                return newChat.id;
 
 
-    },
-    addNewMessage:(chatId:string, message:Message) =>{
-        set((state)=>({
-            chatHistory:state.chatHistory.map((chat)=>
-                chat.id === chatId ? {...chat, message:[...chat.messages, message]}:chat
-            )
-        }))
-    }
-}))
+            },
+            addNewMessage: (chatId: string, message: Message) => {
+                set((state) => ({
+                    chatHistory: state.chatHistory.map((chat) =>
+                        chat.id === chatId ? { ...chat, messages: [...chat.messages, message] } : chat
+                    )
+                }))
+            }
+        })
+        , {
+            name: "chat-storage",
+            storage: createJSONStorage(() => AsyncStorage)
+        })
+);
